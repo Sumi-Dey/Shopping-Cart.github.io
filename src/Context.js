4f954f9c285093f30186fa05f838d864a50ee425
Sumi Dey <dsumi4161@gmail.com>
2023-01-11 19:58:46 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,14 +1,17 @@
-import React, { createContext, useReducer, useState, useContext } from 'react';
-import { productReducer } from './State/Reducers/productReducer';
+import React, { createContext, useContext, useReducer, useState } from 'react';
+import { product } from './Data/Products';
+import reducers from './State/Reducers/Index';
+
 export const Cart = createContext();
 
 function Context({children}) {
-    const[cart,setCart] = useState([])
-    const [productState,productDispatch] = useReducer(productReducer,{
-      byRating:0,
+    const [cart,setCart]= useState([])
+    const [state,dispatch] = useReducer( reducers ,{
+      Products:product,
+      
     })
   return (
-    <Cart.Provider value={{cart,setCart,productState,productDispatch}}>
+    <Cart.Provider value={{...state,dispatch,cart,setCart}}>
         {children}
     </Cart.Provider>
   )
