4fd04b6cdb18ce1c32d3e60798ac77527328517d
Sumi Dey <dsumi4161@gmail.com>
2023-01-11 19:57:06 +0530

    Update SingleProduct.js

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,38 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Cart } from '../Context';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../State/Index';
-// import Rating from './Rating';
-function SingleProduct({ prod }) {
-    const { cart, setCart } = useContext(Cart);
+
+
+function SingleProducts({ prod }) {
+    const { cart,setCart } = useContext(Cart);
     const dispatch = useDispatch();
     const { addToCart, removeFromCart } = bindActionCreators(actionCreators, dispatch)
 
     const addPrice = () => {
         addToCart(prod.price)
-        setCart([...cart, prod])
+        setCart([...cart,prod])
     }
+
     const removePrice = () => {
         removeFromCart(prod.price)
         setCart(cart.filter((c) => c.price !== prod.price))
     }
     return (
-        <div>
-            <div className="card" style={{ width: "18rem" }}>
+        <div className='wholecard'>
+            <div className="card" style={{ width: "17rem" }}>
                 <img src={prod.image} style={{ height: "25rem" }} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-title">{prod.title.slice(0, 25)}...</p>
-                    <div>
+                     <div>
                         <h6 className="card-title">{prod.price}</h6>
-                        {cart.includes(prod) ? <button to="/" className="btn btn-danger btn-sm" onClick={removePrice}>Remove from cart</button> :
-                            <button to="/" className="btn btn-primary btn-sm" onClick={addPrice} >Add to Cart</button>}
-                    </div>
-                    {/* <Rating Rating={prod.rating}  /> */}
+                        
+                            {cart.includes(prod)?<button to="/" className="btn btn-danger btn-sm" style={{backgroundColor:"#EB455F",border:'none'}} onClick={removePrice}>Remove from cart</button>:
+                             <button to="/" className="btn btn-primary btn-sm" style={{backgroundColor:"rgb(251 103 126)",border:'none'}} onClick={addPrice} >Add to Cart</button>}
+                             
+                            
+                    </div> 
                 </div>
             </div>
         </div>
     )
 }
 
-export default SingleProduct;
+export default SingleProducts;
